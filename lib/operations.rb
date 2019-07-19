30168e33c9dd59017304792a293b892fd1ce5563
Jason Viola <jason.viola@mavs.uta.edu>
2019-07-19 21:20:05 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 60
+    p true
+  elsif speed < 40
+    p true
+  else
+    p false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+speed > 60 || speed < 40 ? true : false
 end
-	
+
 
 
